8d7039a631f839a00dc94e1985b221c4fc0abdd7
eunseokyang <54825838+eunseokyang@users.noreply.github.com>
2021-02-02 13:34:36 +0900

    add telebot alarms

--- alarm.py
+++ alarm.py
@@ -1,10 +1,30 @@
 from telegram.ext import Updater
+from telegram.ext import CommandHandler
 
 import config
 
 class TelegramBot:
     def __init__(self):
-        self.updater = Updater(config.TG_API)
+        # self.updater = Updater(config.TG_API)
+        self.updater = Updater(config.TG_TEST_API)
+        self.dispatcher = self.updater.dispatcher
+
+        self.stop_handler = CommandHandler('stop', self.stop)
+        self.start_handler = CommandHandler('restart', self.restart)
+
+        self.dispatcher.add_handler(self.stop_handler)
+        self.dispatcher.add_handler(self.start_handler)
+        self.is_stopped = False
+
+        self.updater.start_polling()
     
     def send_msg(self, text):
         self.updater.bot.send_message(chat_id=config.TG_ID, text=text)
+
+    def stop(self, update, context):
+        self.is_stopped = True
+        context.bot.send_message(chat_id=config.TG_ID, text='[Alarm] All stopped')
+        
+    def restart(self, update, context):
+        self.is_stopped = False
+        context.bot.send_message(chat_id=config.TG_ID, text='[Alarm] All restarted')
--- test_run.py
+++ test_run.py
@@ -3,12 +3,10 @@ import json
 import websocket
 
 import alarm
-import trade
 
-SOCKET = "wss://fstream.binance.com/ws/btcusdt@kline_5m"
+SOCKET = "wss://fstream.binance.com/ws/btcusdt@kline_1m"
 
 telebot = alarm.TelegramBot()
-bn = trade.Trade(telebot)
 
 def on_open(ws):
     print("opened connection")
@@ -19,8 +17,14 @@ def on_close(ws):
 def on_message(ws, message):
     if telebot.is_stopped == False:
         json_message = json.loads(message)
-        if abs(time.time() - json_message["E"]/1000) < 2:
-            bn.run(json_message)
+
+        # if abs(time.time() - json_message["E"]/1000) < 2:
+        #     telebot.send_msg(json_message)
+        #     time.sleep(1)
+
+        telebot.send_msg(json_message)
+        telebot.send_msg(time.time())
+        time.sleep(1)
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
